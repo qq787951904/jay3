9818f0c7e1d61198a9e09eb0387230f4eca5dfa8
qq787951904 <787951904@qq.com>
2019-08-20 17:23:37 +0800

    Initial commit

---
+++ src/learnThread/CallerTask.java
@@ -0,0 +1,27 @@
+package learnThread;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
+public class CallerTask implements Callable<String> {
+
+    @Override
+    public String call() throws  Exception{
+
+        return "Hello";
+    }
+
+    public static void main(String[] args) throws InterruptedException{
+
+        FutureTask<String> futureTask = new FutureTask<>(new CallerTask());
+        new Thread(futureTask).start();
+        try{
+            String result = futureTask.get();
+            System.out.println(result);
+        }catch (ExecutionException e){
+            e.printStackTrace();
+        }
+    }
+}
+
